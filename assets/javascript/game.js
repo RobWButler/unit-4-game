6f8c691345452f3759aa4c40615a99bd544d6e15
Robert Butler <robwbutler@gmail.com>
2019-04-24 00:52:50 -0400

    massive refactoring progress using contructors

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,75 +4,112 @@ var gameoverMusic = new Audio("assets/sound/2-27 - Miss.mp3")
 
 var musicOn = false;
 
-var player = {
-    name: "",
-    hp: "",
-    attack: "",
-    defense: "",
-    isBlock: false,
-    isBuff: false,
-    isLive: true,
-    alreadyBuff: false,
-};
+var Character = function (name, hp, attack, defense, choice){
+    this.name = name;
+    this.hp = parseInt(hp);
+    this.atk = parseInt(attack);
+    this.defense = parseInt(defense);
+    this.choice = choice;
+    this.isBlock = false;
+    this.isBuff= false;
+    this.isLive = true;
 
-var enemy = {
-    name: "",
-    hp: "",
-    attack: "",
-    defense: "",
-    isBlock: false,
-    isBuff: false,
-    isLive: true,
-    alreadyBuff: false,
-};
+    this.attack = function(y) {
+        damage = this.atk - y.defense
 
-var char_1 = {
-    name: "mario",
-    hp: 200,
-    attack: 50,
-    defense: 20,
-    isBlock: false,
-    isBuff: false,
-    isLive: true,
-    alreadyBuff: false,
-    choice: 1,
+    if (damage < 0) {
+        damage = 0;
+    }
+
+    y.hp -= damage;
+
+    $("#player-hp").text(this.hp);
+    $("#enemy-hp").text(y.hp);
+    
+        if (this.isBuff) {
+            this.isBuff = false;
+            this.atk /= 2;
+            $("#player-atk").text(this.atk);
+            $("#enemy-atk").text(this.atk);
+
+        };
+    };
+    
+    this.block = function () {
+        this.isBlock = true;
+        this.defense *= 2.5;
+    };
+    
+    this.atkbuff = function() {
+        if (this.isBuff) {
+        
+        }
+    
+        else {
+            this.isBuff = true;
+            this.atk *= 2.5;
+        }
+    };
+
+    this.turnEnd = function(y) {
+        if (this.isBlock) {
+            this.defense /= 2.5;
+            console.log(this.defense)
+        }
+    
+        if (y.isBlock) {
+            y.defense /= 2.5;
+            console.log(y.defense)
+        }
+    
+        this.isBlock = false
+        y.isBlock = false
+    
+        $("#player-def").text(player.defense);
+    
+        if (player.hp <= 0) {
+            $("#actiontext").text("You lose!")
+            player.isLive = false;
+            $("#buttonspace").toggle()
+            $("#player-hp").html("<i class='fas fa-skull'></i>")
+            $("#instructions").text("GAME OVER!")
+            playMusic.pause();
+            gameoverMusic.play();
+            checkDeath(player.name)
+        }
+    
+        if (enemy.hp <= 0) {
+            $("#enemyactiontext").text(enemy.name.charAt(0).toUpperCase()  + enemy.name.slice(1) + " was defeated! You leveled up!")
+            player.atk = player.atk + 10;
+            $("#player-atk").text(" " + player.atk);
+            player.defense = player.defense + 10;
+            $("#player-def").text(" " + player.defense);
+            enemy.isLive = false;
+            $("#buttonspace").toggle()
+            $("#instructions").text("Select your opponent!")
+            $("#enemy-hp").html("<i class='fas fa-skull'></i>")
+            score++
+    
+            checkDeath(enemy.name)
+        }
+    
+    
+        if (score >= 3) {
+            $("#instructions").text("You win!")
+            playMusic.pause();
+            winMusic.play();
+    
+        }
+        
+    };
 }
 
-var char_2 = {
-    name: "luigi",
-    hp: 210,
-    attack: 50,
-    defense: 15,
-    isBlock: false,
-    isBuff: false,
-    isLive: true,
-    alreadyBuff: false,
-    choice: 2,
-};
-
-var char_3 = {
-    name: "peach",
-    hp: 220,
-    attack: 40,
-    defense: 25,
-    isBlock: false,
-    isBuff: false,
-    isLive: true,
-    alreadyBuff: false,
-    choice: 3,
-};
-
-var char_4 = {
-    name: "bowser",
-    hp: 230,
-    attack: 60,
-    defense: 10,
-    isBlock: false,
-    isBuff: false,
-    isLive: true,
-    alreadyBuff: false,
-    choice: 4,
-};
+var mario = new Character("Mario", 200, 50, 20, 1);
+var luigi = new Character("Luigi", 210, 50, 15, 2);
+var peach = new Character("Peach", 220, 40, 25, 3);
+var bowser = new Character("Bowser", 230, 60, 10, 4);
+var player = new Character("", "", "", "");
+var enemy = new Character("", "", "", "");
 
 var damage = "";
 
@@ -81,45 +118,7 @@ var enemyChoice = false;
 
 var score = 0;
 
-function attack(x, y) {
 
-    damage = x.attack - y.defense
-
-    if (damage < 0) {
-        damage = 0
-    }
-
-    y.hp = y.hp - damage
-
-    $("#player-hp").text(player.hp);
-    $("#enemy-hp").text(enemy.hp);
-    
-    if (x.isBuff) {
-        x.isBuff = false;
-        x.attack = x.attack / 2
-        $("#player-atk").text(player.attack);
-        $("#enemy-atk").text(enemy.attack);
-
-    }
-    
-};
-
-function block(x) {
-    x.isBlock = true
-    x.defense = x.defense * 2.5
-};
-
-function atkbuff(x) {
-    if (x.isBuff) {
-        x.alreadyBuff = true
-    }
-
-    else {
-        x.isBuff = true
-        x.attack = x.attack * 2
-        x.alreadyBuff = true
-    }
-};
 
 function enemyTurn(event) {
     
@@ -134,19 +133,19 @@ function enemyTurn(event) {
 
 
     if (enemyAct === "attack") {
-        attack(enemy, player);
+        enemy.attack(player);
         $("#enemyactiontext").text(enemy.name.charAt(0).toUpperCase()  + enemy.name.slice(1) + " counter-attacked for " + damage + " damage!")
         $("#player-hp").text(" " + player.hp)
     }
 
     if (enemyAct === "atkbuff") {
-        atkbuff(enemy);
+        enemy.atkbuff();
         $("#enemyactiontext").text(enemy.name.charAt(0).toUpperCase()  + enemy.name.slice(1) + " powered up their next attack!")
-        $("#enemy-atk").text(" " + enemy.attack)
+        $("#enemy-atk").text(" " + enemy.atk)
     }
 
     if (enemyAct === "block") {
-        block(enemy);
+        enemy.block();
         $("#enemyactiontext").text(enemy.name.charAt(0).toUpperCase()  + enemy.name.slice(1) + " blocked some damage from your attack!")
     }
 }
@@ -160,71 +159,21 @@ function checkDeath(charname){
     }
 }
 
-function turnEnd(x, y) {
-    if (x.isBlock) {
-        x.defense = x.defense / 2.5
-    }
-
-    if (y.isBlock) {
-        y.defense = y.defense / 2.5
-    }
-
-    x.isBlock = false
-    y.isBlock = false
-
-    $("#player-def").text(player.defense);
-
-    if (player.hp <= 0) {
-        $("#actiontext").text("You lose!")
-        player.isLive = false;
-        $("#buttonspace").toggle()
-        $("#player-hp").html("<i class='fas fa-skull'></i>")
-        $("#instructions").text("GAME OVER!")
-        playMusic.pause();
-        gameoverMusic.play();
-        checkDeath(player.name)
-    }
-
-    if (enemy.hp <= 0) {
-        $("#enemyactiontext").text(enemy.name.charAt(0).toUpperCase()  + enemy.name.slice(1) + " was defeated! You leveled up!")
-        player.attack = player.attack + 10;
-        $("#player-atk").text(" " + player.attack);
-        player.defense = player.defense + 10;
-        $("#player-def").text(" " + player.defense);
-        enemy.isLive = false;
-        $("#buttonspace").toggle()
-        $("#instructions").text("Select your opponent!")
-        $("#enemy-hp").html("<i class='fas fa-skull'></i>")
-        score++
-
-        checkDeath(enemy.name)
-    }
-
-
-    if (score >= 3) {
-        $("#instructions").text("You win!")
-        playMusic.pause();
-        winMusic.play();
-
-    }
-    
-};
-
 
 $("#player-hp").text(" " + player.hp);
-$("#player-atk").text(" " + player.attack);
+$("#player-atk").text(" " + player.atk);
 $("#player-def").text(" " + player.defense);
 
 $("#enemy-hp").text(" " + enemy.hp);
-$("#enemy-atk").text(" " + enemy.attack);
+$("#enemy-atk").text(" " + enemy.atk);
 $("#enemy-def").text(" " + enemy.defense);
 
 $("#btn-atk").click(function(){
     enemyTurn()
-    attack(player, enemy)
+    player.attack(enemy)
     $("#actiontext").text("You attacked " + enemy.name.charAt(0).toUpperCase()  + enemy.name.slice(1) + " for " + damage + " damage!")
     player.alreadyBuff = false;
-    turnEnd(player, enemy)
+    player.turnEnd(enemy)
     
 });
 
@@ -232,52 +181,52 @@ $("#btn-buff").click(function(){
 
     if (player.alreadyBuff) {
         $("#actiontext").text("Can't power up twice in a row!")
-        $("#player-atk").text(" " + player.attack);
+        $("#player-atk").text(" " + player.atk);
         enemyTurn();
-        turnEnd(player, enemy)
+        player.turnEnd(enemy)
     }
 
     else {
-        atkbuff(player)
+        player.atkbuff()
         $("#actiontext").text("You strengthened your attack for next turn!")
-        $("#player-atk").text(" " + player.attack);
+        $("#player-atk").text(" " + player.atk);
         enemyTurn();
-        turnEnd(player, enemy)
+        player.turnEnd(enemy)
     }
 });
 
 $("#btn-blk").click(function(){
-    block(player)
+    player.block()
     $("#actiontext").text("You blocked some incoming damage!")
     enemyTurn();
-    turnEnd(player, enemy)
+    player.turnEnd(enemy)
 });
 
 
 $("#choice1").click(function(){
-    selectCharacter(char_1.name, char_1)
+    selectCharacter(mario.name, mario)
 })
 
 $("#choice2").click(function(){
-    selectCharacter(char_2.name, char_2)
+    selectCharacter(luigi.name, luigi)
 })
 
 $("#choice3").click(function(){
-    selectCharacter(char_3.name, char_3)
+    selectCharacter(peach.name, peach)
 
 })
 
 $("#choice4").click(function(){
-    selectCharacter(char_4.name, char_4)
+    selectCharacter(bowser.name, bowser)
 })
 
 function selectCharacter(charname, charnum) {
-    if (playerChoice != true) {
+    if (!playerChoice) {
         for(var k in charnum) (player[k] = charnum[k])
         $("#playericon").attr("src", "assets/images/" + charname + ".png");
         $("#playericon").css('visibility', 'visible');
         $("#player-hp").text(" " + player.hp);
-        $("#player-atk").text(" " + player.attack);
+        $("#player-atk").text(" " + player.atk);
         $("#player-def").text(" " + player.defense);
         $("#playername").text(player.name.charAt(0).toUpperCase()  + player.name.slice(1));
         $("#player_sprite").append("<img src=assets/images/" + charname + "idle.gif>")
@@ -290,12 +239,12 @@ function selectCharacter(charname, charnum) {
 
     }
     
-    else if (enemyChoice !=true && enemy.isLive) {
+    else if (!enemyChoice && enemy.isLive) {
         for(var k in charnum) (enemy[k] = charnum[k])
         $("#enemyicon").attr("src", "assets/images/" + charname + ".png");
         $("#enemyicon").css('visibility', 'visible');
         $("#enemy-hp").text(" " + enemy.hp);
-        $("#enemy-atk").text(" " + enemy.attack);
+        $("#enemy-atk").text(" " + enemy.atk);
         $("#enemy-def").text(" " + enemy.defense);
         $("#enemyname").html(enemy.name.charAt(0).toUpperCase()  + enemy.name.slice(1));
         $("#enemy_sprite").html("<img src=assets/images/" + charname + "idle.gif>")
@@ -307,21 +256,17 @@ function selectCharacter(charname, charnum) {
 
     }
 
-    else if (enemyChoice && enemy.isLive != true) {
+    else if (enemyChoice && !enemy.isLive) {
         for(var k in charnum) (enemy[k] = charnum[k])
         $("#enemyicon").attr("src", "assets/images/" + charname + ".png");
         $("#enemy-hp").text(enemy.hp);
-        $("#enemy-atk").text(enemy.attack);
+        $("#enemy-atk").text(enemy.atk);
         $("#enemy-def").text(enemy.defense);
         $("#enemyname").html(enemy.name.charAt(0).toUpperCase()  + enemy.name.slice(1));
         $("#enemy_sprite").html("<img src=assets/images/" + charname + "idle.gif>")
         $("#choice" + charnum.choice).toggle()
         $("#instructions").text("Battle!")
         $("#buttonspace").toggle()
-
-    }
-
-    else {
 
     }
 }
